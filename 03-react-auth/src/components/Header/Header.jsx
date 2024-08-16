f4ff1ae4b2b2d38f8444493b448fa1d660ba6da1
César Guerra <warderer@gmail.com>
2024-08-15 20:59:05 -0600

    💄 Estilo linkIsActive en Header

--- 03-react-auth/src/components/Header/Header.jsx
+++ 03-react-auth/src/components/Header/Header.jsx
@@ -2,24 +2,45 @@ import { NavLink } from 'react-router-dom'
 import './header.scss'
 
 const Header = () => {
+  const linkIsActive = (isActive) => isActive ? 'header__item-link header__item-link--is-active' : 'header__item-link'
+
   return (
     <nav className='header'>
         <NavLink to='/' className='header__logo'>LOGO</NavLink>
         <ul className='header__nav-list'>
             <li className='header__list-item'>
-                <NavLink to="/" className='header__item-link header__item-link--is-active'>Home</NavLink>
+                <NavLink
+                    to="/"
+                    className={({ isActive }) => linkIsActive(isActive)}
+                >Home
+                </NavLink>
             </li>
             <li className='header__list-item'>
-                <NavLink to="/dashboard" className='header__item-link'>Dashboard</NavLink>
+                <NavLink
+                  to="/dashboard"
+                  className={({ isActive }) => linkIsActive(isActive)}>Dashboard
+                </NavLink>
             </li>
             <li className='header__list-item'>
-                <NavLink to="/secret" className='header__item-link'>Secret</NavLink>
+                <NavLink
+                  to="/secret"
+                  className={({ isActive }) => linkIsActive(isActive)}
+                >Secret
+                </NavLink>
             </li>
             <li className='header__list-item'>
-                <NavLink to="/login" className='header__item-link'>Login</NavLink>
+                <NavLink
+                  to="/login"
+                  className={({ isActive }) => linkIsActive(isActive)}
+                >Login
+                </NavLink>
             </li>
             <li className='header__list-item'>
-                <NavLink to="/signup" className='header__item-link'>Signup</NavLink>
+                <NavLink
+                  to="/signup"
+                  className={({ isActive }) => linkIsActive(isActive)}
+                >Signup
+                </NavLink>
             </li>
         </ul>
     </nav>
