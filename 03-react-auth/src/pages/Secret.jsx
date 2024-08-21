00bf90404aeafd8d11698fa6369135a70142302b
César Guerra <warderer@gmail.com>
2024-08-20 21:31:20 -0600

    ✨ Renderizado condicional segun role

--- 03-react-auth/src/pages/Secret.jsx
+++ 03-react-auth/src/pages/Secret.jsx
@@ -1,6 +1,17 @@
+import { useAuthContext } from "../hooks/useAuth"
+
 const Secret = () => {
+  const { userPayload } = useAuthContext()
+
   return (
-    <h1>Secret</h1>
+    <>
+      <h1>Secret</h1>
+      {
+        userPayload?.role == 'ADMIN'
+          ? <h2>Hola Admin</h2>
+          : <h2>Hola Customer</h2>
+      }
+    </>
   )
 }
 export default Secret
