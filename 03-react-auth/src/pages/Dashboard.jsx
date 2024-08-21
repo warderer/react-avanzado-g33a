4751cd835ca8d7d0c75be925a76ab83f84b52294
César Guerra <warderer@gmail.com>
2024-08-20 21:37:59 -0600

    ✨ userProfile en Dashboard

--- 03-react-auth/src/pages/Dashboard.jsx
+++ 03-react-auth/src/pages/Dashboard.jsx
@@ -1,6 +1,31 @@
+import { useState, useEffect } from 'react'
+import { getMeUserService } from '../services/userServices'
+
 const Dashboard = () => {
+  const [userData, setUserData] = useState({})
+  const token = localStorage.getItem('token')
+
+  useEffect(() => {
+    const getUserData = async () => {
+      try {
+        const response = await getMeUserService(token)
+        setUserData(response.data)
+      } catch (error) {
+          console.error('Ocurrio un error al obtener los datos del usuario', error.message)
+      }
+    }
+    getUserData()
+  }, [token])
+
   return (
-    <h1>Dashboard</h1>
+    <>
+      <h1>Dashboard</h1>
+      {userData?.first_name && <p>Welcome {userData.first_name}</p>}
+      {userData?.last_name && <p>{userData.last_name}</p>}
+      {userData?.email && <p>{userData.email}</p>}
+      {userData?.role && <p>{userData.role}</p>}
+      {userData?.gender && <p>{userData.gender}</p>}
+    </>
   )
 }
 export default Dashboard
--- 03-react-auth/src/services/userServices.js
+++ 03-react-auth/src/services/userServices.js
@@ -4,8 +4,14 @@ const BASE_URL = 'https://ecommerce-json-jwt.onrender.com'
 
 const registerUserService = (data) => axios.post(`${BASE_URL}/register`, data)
 const loginUserService = (data) => axios.post(`${BASE_URL}/login`, data)
+const getMeUserService = (jwt) => axios.get(`${BASE_URL}/users/me`, {
+    headers: {
+        Authorization: `Bearer ${jwt}`
+    }
+})
 
 export {
     registerUserService,
-    loginUserService
+    loginUserService,
+    getMeUserService
 }
