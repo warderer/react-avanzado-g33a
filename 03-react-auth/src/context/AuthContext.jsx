dcfdfd3e123d42622dd5b8cf600b69f64b79563f
César Guerra <warderer@gmail.com>
2024-08-20 20:04:50 -0600

    ✨ AuthContext con Login y Logout

--- 03-react-auth/src/context/AuthContext.jsx
+++ 03-react-auth/src/context/AuthContext.jsx
@@ -1,4 +1,5 @@
-import { createContext } from 'react'
+import { createContext, useState } from 'react'
+import { jwtDecode } from 'jwt-decode'
 
 // #1 Crear el contexto
 const AuthContext = createContext()
@@ -6,10 +7,25 @@ const AuthContext = createContext()
 // #2 Crear la función que proveerá el contexto (provider)
 const AuthProvider = ({ children }) => {
 // Acá va mi lógica del proveedor
+    const [isAuth, setIsAuth] = useState(false) // ¿Estoy autenticado?
+    const [userPayload, setUserPayload] = useState(null) // ¿Quién soy?: JWT payload
 
-const data = {
+    const login = (jwtToken) => {
+        localStorage.setItem('token', jwtToken)
+        const payload = jwtDecode(token)
+        setIsAuth(true)
+        setUserPayload(payload)
+    }
+
+    const logout = () => {
+        localStorage.removeItem('token')
+        setIsAuth(false)
+        setUserPayload(null)
+    }
+
+    const data = {
     // Las cosas que queremos compartir
-}
+    }
     return(
         <AuthContext.Provider value={data}>
             {children}
