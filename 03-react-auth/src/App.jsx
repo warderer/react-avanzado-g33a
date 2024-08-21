b06d9d75db300c690aaed43ca3779ae79921707d
César Guerra <warderer@gmail.com>
2024-08-20 20:42:52 -0600

    ✨ AuthContext implementado en App

--- 03-react-auth/src/App.jsx
+++ 03-react-auth/src/App.jsx
@@ -1,16 +1,19 @@
 import { BrowserRouter } from 'react-router-dom'
+import { AuthProvider } from './context/AuthContext'
 import Header from './components/Header'
 import RoutesIndex from './routes/RoutesIndex'
 import './App.css'
 
 function App() {
-
+/* 2b. Implementar el contexto en mi App */
   return (
     <>
-    <BrowserRouter>
-      <Header />
-      <RoutesIndex />
-    </BrowserRouter>
+    <AuthProvider>
+      <BrowserRouter>
+        <Header />
+        <RoutesIndex />
+      </BrowserRouter>
+    </AuthProvider>
     </>
   )
 }
